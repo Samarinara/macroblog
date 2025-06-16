24b859f256d281fdbf689bbe20761f6bad7d47fb
Samarinara <samkatevatis@gmail.com>
2025-06-16 15:18:06 -0700

    class -> className

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {Card} from '@/components/ui/card'
 function App() {
   return (
     <>
-        <div class="flex items-center justify-center h-screen" >
+        <div className="flex items-center justify-center h-screen" >
           <div >
             <Card id='main-card'>
               <h1>M A C R O B L O G</h1>
@@ -18,8 +18,8 @@ function App() {
               <Button id='resizable-button'>Read More</Button>
             </Card>
           </div>
-        </div> 
-        <div class="grid-bg"></div>
+        </div>
+        <div className="grid-bg"></div>
     </>
   )
 }
